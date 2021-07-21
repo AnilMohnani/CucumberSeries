c310950baab0f0d326d0ec2e4e64b1dd9dc7e576
grasshopper7 <grass.hopper.moc@gmail.com>
2021-07-21 18:13:33 +0530

    Added pdf report. Changed pom.

--- src/test/java/testRunner/RunnerTest.java
+++ src/test/java/testRunner/RunnerTest.java
@@ -4,8 +4,11 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src//test//resources//features"},glue= {"stepDefinitions","appHooks"},plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","timeline:test-output-thread/"},monochrome=true, publish=true)
+@CucumberOptions(features = { "src//test//resources//features" }, glue = { "stepDefinitions", "appHooks" }, plugin = {
+		"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"timeline:test-output-thread/" }/* , monochrome = true, publish = true */)
 public class RunnerTest {
 
 }
